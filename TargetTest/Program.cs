4fbe9d5352ea8f930eaaf2e37120e1a4e8a295f0
Lucas Vidotto <lucasvidotto12@gmail.com>
2024-09-19 21:09:03 -0300

    upload da criação do projeto + exercício 1

---
+++ TargetTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TargetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 12;
+            int SOMA = 0;
+            int K = 1;
+
+            while (K < INDICE)
+            {
+                K++;
+                SOMA = Aggregate(SOMA,K);
+                //K = K + 1;
+                //SOMA = SOMA + K;
+            }
+
+            Console.WriteLine(SOMA);
+        }
+
+        internal static int Aggregate(int FirstValue, int SecondValue)
+        {
+            return FirstValue += SecondValue;
+        }
+    }
+}
